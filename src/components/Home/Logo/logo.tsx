8682734ad5984d0d473df12fd15280422e53b683
corrado <corrado@infostreet.com>
2024-08-24 20:14:26 -0700

    cleaned up animation and created logo

---
+++ src/components/Home/Logo/logo.tsx
@@ -0,0 +1,17 @@
+import './logo.scss';
+import { useEffect, useRef } from 'react';
+
+
+
+export const Logo: React.FC = () => {
+
+    useEffect(() => {
+
+    })
+
+    return (
+        <div className='logo-container' >
+            <h1>some sort of logo here</h1>
+        </div>
+    )
+}
--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './home.scss';
 import { AnimatedLetters } from '../AnimatedLetters/animatedLetters';
 import { Link } from 'react-router-dom';
+import { Logo } from './Logo/logo';
 
 export const Home: React.FC = () => {
     const [lettersAnimation, setLettersAnimation] = useState('text-animate');
     const greetings = 'Hey, there'.split('');
     const fName = `I'm Corrado`.split('');
     const job = 'Software Developer'.split('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLettersAnimation('text-animate-hover')   
+        }, 4000);
+    }, [])
 
     return (
         <div className='container home-page'>
@@ -16,7 +23,7 @@ export const Home: React.FC = () => {
                     <AnimatedLetters 
                         letters={lettersAnimation}
                         message={greetings}
-                        idx={1}
+                        idx={10}
                     />
                     <br/>
                     <AnimatedLetters 
@@ -29,15 +36,17 @@ export const Home: React.FC = () => {
                     <AnimatedLetters 
                         letters={lettersAnimation}
                         message={job}
-                        idx={40}
+                        idx={31}
                     />
                 </h1>
-                <h2>Full Stack Developer / Mobile Developer / TV App Developer</h2>
-                <Link to="/contact" className="flat-button">
-                    CONTACT ME
-                </Link>
+                <div className='sub-description-animation'>
+                    <h2>Full Stack Developer / Mobile Developer / TV App Developer</h2>
+                    <Link to="/contact" className="flat-button">
+                        CONTACT ME
+                    </Link>
+                </div>
             </div>
-            
+            <Logo />
         </div>
     )
 }
